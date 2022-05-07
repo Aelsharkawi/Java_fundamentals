e98afc16d6d6904c8d384021fbac95e8d3b229ab
Abdullah Elsharkawi <tester@Abdullahs-MacBook-Pro.local>
2022-05-07 23:09:35 +0200

    solve exercise

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -16,7 +16,24 @@ public class Exercise_04 {
         System.out.println("int i is: " + i);
 
         // write your code below
-
+        byte b = 1;
+        System.out.println("byte b is: " + b);
+        short s = 22;
+        System.out.println("Short s is: " + s);
+        boolean bool = false;
+        System.out.println("Boolean bool is: " + bool);
+        int k = 55;
+        System.out.println("Int k is: " + k);
+        long l = 6589544;
+        System.out.println("Long l is: " + l);
+        double d = 5.5d;
+        System.out.println("Double d is: " + d);
+        float f = 6f;
+        System.out.println("Float f is: " + f);
+        char c = 'l';
+        System.out.println("Char c is: " + c);
+        String str = "Am Abdullah";
+        System.out.println("String str is:" + str);
     }
 
 }
--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -17,12 +17,20 @@ public class Exercise_05 {
 
         String str2 = "hello";
         // please initialize a boolean variable and test whether str is equal to str2
-
+        boolean bool = true;
+        bool = str.equalsIgnoreCase(str2);
+        System.out.println("Is str is equal to str2: " + bool);
         // please concatenate str & str2 and set the result to a new String variable below
-
+        String newstr = "";
+        newstr = str + str2;
+        System.out.println(newstr);
         // please demonstrate the use of any other method that is available to us in the String class
-        // for example, replace(), substring(), contains(), indexOf() etc
-
+        // for example, replac e(), substring(), contains(), indexOf() etc
+        String replaceString = str.replace("l","p");
+        System.out.println("Str after replace: " + replaceString);
+        System.out.println("Substring newstr from index 6: " + newstr.substring(6));
+        System.out.println(newstr.contains("M"));
+        System.out.println(newstr.indexOf('l'));
     }
 
 
